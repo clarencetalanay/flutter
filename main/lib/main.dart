3081445599ac249b4c79ee5b69d72b08e399c4be
clarencetalanay <clarence.talanay@gmail.com>
2026-09-11 09:21:55 +0000

    meow

--- main/lib/main.dart
+++ main/lib/main.dart
@@ -12,7 +12,53 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           home: Scaffold(
             body: Center(
-              child: Text('Hi pogi')
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_circle_rounded, color: Colors.deepPurple, size: 145,
+                  ),
+                  Text(
+                    'Clarence Talanay', style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold, color: Colors.black)
+                  ),
+                  Text('Bachelor of Science in Information Technology', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey)
+                  ),
+                  SizedBox(height:150),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.home_outlined, color: Colors.deepPurple, size: 45,
+                      ),
+                      Text(
+                        'Home', style: TextStyle(fontSize: 21, color: Colors.grey)
+                      ),
+                    ],
+                  ),
+                  SizedBox(height:150),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.menu_book, color: Colors.deepPurple, size: 45,
+                      ),
+                      Text(
+                        'Courses', style: TextStyle(fontSize: 21, color: Colors.grey)
+                      ),
+                    ],
+                  ),
+                  SizedBox(height:150),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.person_2_outlined, color: Colors.deepPurple, size: 45,
+                      ),
+                      Text(
+                        'Profile', style: TextStyle(fontSize: 21, color: Colors.grey)
+                      ),
+                    ],
+                  ),
+                   
+                ],
+              ),
             ),
           ),
         );
